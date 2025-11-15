02aaf55d4f8a56c4ed64bc25760be33fbe9bc357
elizabethtabitha1310 <tabyeliza@gmail.com>
2025-11-15 03:49:45 +0300

    Home page

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/app/sections/Hero/Hero";
+import SummaryCards from "@/components/SummaryCards";
+
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <SummaryCards/>
+    </main>
+  );
+}
